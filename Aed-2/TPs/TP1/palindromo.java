d5868cbdf079756f65d90d8665105d010ad6ea6a
Felipe Nepomuceno Coelho <65971797+FelipeN21@users.noreply.github.com>
2021-08-17 09:11:13 -0300

    Variables Improvement

--- Aed-2/TPs/TP1/palindromo.java
+++ Aed-2/TPs/TP1/palindromo.java
@@ -2,12 +2,12 @@
 
 class palindromo {
   public static void main(String[] args) { // le os dados e verifica a flag
-    boolean pa = true;
+    boolean notFim = true;
     boolean vef = true;
     String y = MyIO.readLine();
-    while (pa == true)
-      if (y.length() == 3 && y.charAt(0) == 'F' && y.charAt(1) == 'I' && y.charAt(2) == 'M') {
-        pa = false;
+    while (notFim == true) //roda enquanto não ocorre a condicao de parada
+      if (y.length() == 3 && y.charAt(0) == 'F' && y.charAt(1) == 'I' && y.charAt(2) == 'M') {//busca a condicao de parada
+        notFim = false;
       } else {
         vef = verificador(y);
         if (vef == true) {
@@ -23,20 +23,20 @@ class palindromo {
   }
 
   public static boolean verificador(String x) { // compara strings inversas para ver se a plavra e ou nao palindromo
-    boolean bbb = true;
+    boolean toReturn = true;
     int i = x.length()-1;
 
     for (int n = 0; n < x.length()-1; n = n + 1) {
 
         if (x.charAt(n) != x.charAt(i)) {
-          bbb = false;
+          toReturn = false;
 
           n = x.length();
         }
         i--;
 
     }
-    return bbb;
+    return toReturn;
 
   }
 
